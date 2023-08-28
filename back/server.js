eade57baec2c5ecbbd8d2708f4f9b5db0f85cf0a
heritsilavo <heritsilavo4835@gmail.com>
2023-08-28 16:08:13 +0300

    utilisation du module socket.io

--- back/server.js
+++ back/server.js
@@ -3,6 +3,34 @@ const cookieParser=require('cookie-parser');
 const cors=require('cors');
 const jwt=require("jsonwebtoken");
 const mysql=require('mysql2/promise');
+const http=require('http');
+const { Server} = require("socket.io");
+const { disconnect } = require('process');
+
+const app=express();
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["POST","G"]
+    }
+})
+
+server.listen(3002,function() {
+    console.log("server running on port 3002(socket.io)");
+})
+
+io.on("connection",function(socket) {
+    console.log("connected as: "+socket.id);
+    socket.on("disconnect", (reason) => {
+        console.log("disconnected because",reason);
+    });
+})
 
 const connInfo={
     host:'localhost',
@@ -11,11 +39,6 @@ const connInfo={
     database:'messagerie'
 }
 
-const app=express();
-
-app.use(cookieParser());
-app.use(cors());
-app.use(express.json());
 
 const secretKey='**$my$*$secret§§=⁺¨^key'
 
--- frontend/src/Components/Connected.jsx
+++ frontend/src/Components/Connected.jsx
@@ -1,8 +1,16 @@
 import Nav from './utilities/Nav';
 import { Outlet } from 'react-router-dom';
+import io from "socket.io-client"
+
+let socket=null;
 
 function Connected() {
+    socket=io.connect("http://localhost:3002/");
+    const disconnect=()=>{
+        socket.disconnect()
+    }
     return<>
+        <button onClick={disconnect}>disconnect</button>
         <Nav></Nav>
         <Outlet></Outlet>
     </>
